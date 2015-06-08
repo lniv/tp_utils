b7e415f4834908e866cad8b9e4daac7cf72812ef
Niv Levy <niv@valis>
2015-06-08 00:46:32 -0700

    Can merge multiple files (or rather open more than one file into a common set), test duplicates, write files (semi tested); no automation of separating files - i think deciding what to do with the duplicates is almost necessarily manual. However, one can already write the unique ones to file using the implemented functions, write the duplicates to a file, clean that file manually, and then reopen the files together and write them to a file (or just merge the two files manually / shell once the deuplicates one is cleaned).

--- turnpoint_utils.py
+++ turnpoint_utils.py
@@ -10,6 +10,8 @@ from numpy import isnan, nan, sin, cos, arctan , pi
 import numpy as np
 from geopy.distance import GreatCircleDistance
 from geopy.point import Point
+from itertools import combinations
+import tempfile, os, copy
 
 #test files:
 # '/home/niv/Downloads/crystal15.dat.dat'
@@ -19,19 +21,28 @@ crystal_airport = Point('34.484885, -117.826479, 3420 ft')
 
 class dat (object):
     """
-    class for storing turnpoint details. The parsed data will be stored as a list where each membr is of he structure of x = { 'point' : (geopy point), 'type' : (landable, turnpoint...), 'comment' : ' '}
+    class for storing turnpoint details. The parsed data will be stored as a list where each membr is of he structure of x = { 'point' : (geopy point), 'type' : (landable, turnpoint...), 'name' : ..., 'comment' : ' '}
     """
     
     def __init__(self, filename):
+	# TODO : modify so that we can easily add a file!
 	self.filename = filename
-	self.parse_dat_file()
+	if type(filename) == str:
+	    self.parse_dat_file(filename)
+	elif type(filename) == list:
+	    #use the shell to concatanate the files; obviously not os agnostic
+	    t = tempfile.NamedTemporaryFile()
+	    print 'cat ' + ' '.join(filename) + ' > ' + t.name
+	    os.system('cat ' + ' '.join(filename) + ' > ' + t.name)
+	    self.parse_dat_file(t.name)
+	    t.close()
 	self.list_turnpoints()
     
-    def parse_dat_file(self):
+    def parse_dat_file(self, filename):
 	"""
 	parse a turnpoint file in dat format
 	"""
-	self.data = pd.read_csv(self.filename, sep = ',', names = ['index', 'Lat', 'Long', 'alt', 'type', 'comment'], comment = '*').dropna(how='all')
+	self.data = pd.read_csv(filename, sep = ',', names = ['index', 'Lat', 'Long', 'alt', 'type', 'name', 'comment'], comment = '*').dropna(how='all')
 	#it's not handling comments properly, which is why trying to dtype it failed, and why the dropna was required.
 	return
 
@@ -64,22 +75,61 @@ class dat (object):
 	d = [''] * len(self.data['alt'])
 	for i, s in enumerate(self.data['alt']):
 	    try:
-		d[i] = s.strip('F') + ' ft'
+		d[i] = str(float(s.strip('F')) * 0.304) + ' m' # seems not to like ft?!
 	    except AttributeError:
 		d[i] = nan
 	self.data['Alt_s'] = np.array(d)
 	self.data['point_s'] = self.data['Lat_s'] + self.data['Long_s'] + self.data['Alt_s']
 	#print data
-	self.turn_points = [{'point' : Point(), 'type' : '', 'comment' : ''}] * len(self.data)
+	self.turn_points = [{'point' : Point(), 'type' : '', 'name' : '', 'comment' : ''}] * len(self.data)
 	for i,s in enumerate(self.data['point_s']):
 	    try:
-		self.turn_points[i] = {'point' : Point(s), 'type' : self.data['type'].iloc[i], 'comment' : self.data['comment'].iloc[i]}
+		if type(self.data['comment'].iloc[i]) != str:
+		    self.turn_points[i] = {'point' : Point(s), 'type' : self.data['type'].iloc[i], 'name' : self.data['name'].iloc[i], 'comment' : ''}
+		else:
+		    self.turn_points[i] = {'point' : Point(s), 'type' : self.data['type'].iloc[i], 'name' : self.data['name'].iloc[i], 'comment' : self.data['comment'].iloc[i]}
 	    except ValueError:
 		print 'Failed to parse ', self.data.iloc[i]
 	# seems pandas cant handle this data type nicely
 	#data['point'] = points
 	return
 
+def format_angle(a, lat_lon):
+    """
+    format angle into deg:min with min having three decimal points
+    """
+    if lat_lon == 'lat':
+	if a < 0:
+	    D = 'S'
+	else:
+	    D = 'N'
+    elif lat_lon == 'lon':
+	if a < 0 :
+	    D = 'W'
+	else:
+	    D = 'E'
+    else:
+	print 'wrong choice'
+	return None
+    return '{:0d}:{:0.3f}'.format(int(a), 60.0 * abs(a - int(a))) + D
+
+#457,33:39.736N,113:45.979W,1175F,L,Vicksburg,15x1500 n/sD levee pwrl Nend emergency
+def tp2dat(filename, turn_points):
+    """
+    write a list of turn points back to a dat file
+    #TODO : check if this is legit, if locations are correct etc
+    """
+    #TODO : check existnece
+    f = open(filename, 'w')
+    for i, tp in enumerate(turn_points):
+	lat = tp['point'].latitude
+	lon = tp['point'].longitude
+	alt = '{:0d}F'.format(int(tp['point'].altitude / 0.304 * 1000.0))
+	try:
+	    f.write(','.join([str(i), format_angle(lat, 'lat'), format_angle(lon, 'lon'), alt, tp['type'], tp['name'], tp['comment']]) + '\n')
+	except TypeError:
+	    print 'failed to print ', tp, ' to file'
+    f.close()
 
 def rad2deg(a):
     return float(a) * 180.0/pi
@@ -88,10 +138,9 @@ def deg2rad(a):
     return float(a) * pi / 180.0
 
 # from http://stackoverflow.com/questions/17624310/geopy-calculating-gps-heading-bearing
-
 def p2p_bearing(pt1, pt2):
     """
-    take two points (in geopy represntation and return a bearing in the range of 0-360 deg
+    take two points (in geopy represntation) and return a bearing in the range of 0-360 deg
     """
     lat1 = deg2rad(pt1.latitude)
     lat2 = deg2rad(pt2.latitude)
@@ -117,17 +166,26 @@ def tp_in_sector(origin, turn_points, direction, width, verbose = True):
     
     if verbose:
 	for tp in sector_pts:      
-	    print tp['comment'], 'bearing = ', p2p_bearing(origin, tp['point']), 'R = ', GreatCircleDistance(origin, tp['point'])
+	    print tp['name'], 'bearing = ', p2p_bearing(origin, tp['point']), 'R = ', GreatCircleDistance(origin, tp['point'])
     return sector_pts
-    
-    
 
 
-def check_for_duplicates(file1, file2, min_distance):
+def check_for_duplicates(files, min_distance = 1.0):
     """
-    take two data objects, and return two - one that is the combination of all unique points in both, and one that contains points which are likely duplicates
+    take one or more files, and return two list of turnpoints- one that is the combination of all unique points in both, and one that contains points which are likely duplicates, based on relative distances
     """
-    data1 = dat(file1)
-    data2 = dat(file2)
-    marge(data1,data2)
+    data = dat(files)
+    unique = copy.deepcopy(data.turn_points)
+    duplicates = []
+    for i, j in combinations(data.turn_points, 2):
+	print 'Testing ', i['name'], j['name']
+	if GreatCircleDistance(i['point'], j['point']) < min_distance:
+	    duplicates.append(i)
+	    duplicates.append(j)
+    for tp in duplicates:
+	try:
+	    unique.remove(tp)
+	except ValueError: # more than two points can share the same space!
+	    print 'Could not remove ', tp
+    return unique, duplicates
     
